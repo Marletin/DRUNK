359a14bd4b078ad4224f5d61aef48ede23b7dad4
Marletin <marletin2@gmail.com>
2017-07-06 21:18:21 +0200

    Added Drunk.php

---
+++ Homepage/Drunk.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8"/>
+			<title>DRUNK - Welcome</title>
+			<link rel="stylesheet" href="test.css"/>
+			<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"/>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+			<script src="test.js"></script>
+		</head>
+		<body>
+			<div id="body_wrapper">
+				<div id="slide">
+					<div id="page_one" class="panel">
+						<h1 id="h_one">DRUNK</h1>
+						<div id="start_arrow"></div>
+					</div>
+					<div id="page_two" class="panel">
+						<h1 id="h_two">DRUNK- Start your Nightlife</h1>
+						<div id="navig">
+							<nav>
+								<ol>
+									<li>Get App</li>
+									<li>Information</li>
+									<li>Contact</li>
+								</ol>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
