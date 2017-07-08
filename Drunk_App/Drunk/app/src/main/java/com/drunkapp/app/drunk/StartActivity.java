db8a93c1028253acdc5d1e3db8dafe16d8af9153
RouladenRobert <jonas.seng1@gmail.com>
2017-07-08 09:28:16 +0200

    Penis

---
+++ Drunk_App/Drunk/app/src/main/java/com/drunkapp/app/drunk/StartActivity.java
@@ -0,0 +1,26 @@
+package com.drunkapp.app.drunk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    static long appStarted = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+
+    protected void startAct(View v)
+    {
+        Intent intent = new Intent(this, MapsTest.class);
+        startActivity(intent);
+        finish();
+    }
+}
