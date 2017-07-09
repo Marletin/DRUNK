94aaab5f6cd9eb90ac31d67e8c24cd724b190a20
Marletin <marletin2@gmail.com>
2017-07-09 13:30:31 +0200

    changes entrypage

--- Drunk_App/Drunk/app/src/main/java/com/drunkapp/app/drunk/StartActivity.java
+++ Drunk_App/Drunk/app/src/main/java/com/drunkapp/app/drunk/StartActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
 
     }
