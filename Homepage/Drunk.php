62c17eb33791ef3e2ceaab41ef6f99d4553e1686
Marletin <marletin2@gmail.com>
2017-08-05 15:46:47 +0200

    Jonas ist  behindert

--- Homepage/Drunk.php
+++ Homepage/Drunk.php
@@ -2,9 +2,9 @@
 	<html>
 		<head>
 			<meta charset="utf-8"/>
-			<title>DRUNK - Welcome</title>
+			<title>DRUNK - Welcome<title>
 			<link rel="stylesheet" href="Drunk.css"/>
-			<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet"> 
+			<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet">
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 			<script src="Drunk.js"></script>
 		</head>
@@ -29,22 +29,23 @@
 						<div id="txt_wrapper">
 							<p id="txt_block_1">
 								Maybe you're asking yourself what is DRUNK? Drunk is an application with which one you can
-								chat with random people to plan something for the night... Going in a pub or in a club? Or 
+								chat with random people to plan something for the night... Going in a pub or in a club? Or
 								just want to chat with random people around you and want to have some fun? Then DRUNK
 								is the app you were searching for!
 							</p>
 						</div>
-						<div id="page_app">
+						<div id="page_app"  class="panel">
 							App
 							<div class="back">Back</div>
 						</div>
 					</div>
-					
-					<div id="page_info">
+
+
+					<div id="page_info"  class="panel">
 						Info
 						<div class="back">Back</div>
 					</div>
-					<div id="page_contact">
+					<div id="page_contact"  class="panel">
 						Contact
 						<div class="back">Back</div>
 					</div>
