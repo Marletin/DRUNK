0f356d9612922f64b4ef224b0c66d5d12d0fbfad
Marletin <marletin2@gmail.com>
2017-09-06 22:20:54 +0200

    COMMIT

--- Homepage/Drunk.php
+++ Homepage/Drunk.php
@@ -2,53 +2,24 @@
 	<html>
 		<head>
 			<meta charset="utf-8"/>
-			<title>DRUNK - Welcome<title>
-			<link rel="stylesheet" href="Drunk.css"/>
-			<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet">
+			<title>DRUNK - Welcome</title>
+			<link rel="stylesheet" href="Test.css"/>
+			<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet"/>
+			<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 			<script src="Drunk.js"></script>
 		</head>
 		<body>
-			<div id="body_wrapper">
-				<div id="slide">
-					<div id="page_one" class="panel">
-						<h1 id="h_one">DRUNK</h1>
-						<div id="start_arrow"></div>
-					</div>
-					<div id="page_two" class="panel">
-						<div id="navig">
-							<nav>
-								<ol>
-									<li id="get_app">Get App</li>
-									<li id="info">Information</li>
-									<li id="contact">Contact</li>
-								</ol>
-							</nav>
-						</div>
-						<h1 id="h_two">DRUNK- Start your Nightlife</h1>
-						<div id="txt_wrapper">
-							<p id="txt_block_1">
-								Maybe you're asking yourself what is DRUNK? Drunk is an application with which one you can
-								chat with random people to plan something for the night... Going in a pub or in a club? Or
-								just want to chat with random people around you and want to have some fun? Then DRUNK
-								is the app you were searching for!
-							</p>
-						</div>
-						<div id="page_app"  class="panel">
-							App
-							<div class="back">Back</div>
-						</div>
-					</div>
+			<div id="main_wrapper">
 
 
-					<div id="page_info"  class="panel">
-						Info
-						<div class="back">Back</div>
+				<div class="mainframe" id="upper">
+					<div>
+
 					</div>
-					<div id="page_contact"  class="panel">
-						Contact
-						<div class="back">Back</div>
-					</div>
+				</div>
+				<div class="mainframe" id="upper">
+
 				</div>
 			</div>
 		</body>
